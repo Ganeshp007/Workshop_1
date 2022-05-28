980283df8785dd38997b3f1a65cd319f3093029b
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-28 10:16:45 +0530

    Added: FindFactorProblem

---
+++ Wokshop-1_PrimeFactor/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int i;
+Console.WriteLine("Enter the number to find prime factors:-");
+int Number=Convert.ToInt32(Console.ReadLine());
+for(i = 1; i <= Number; i++)
+{
+    if(Number%i==0)
+    {
+        Console.WriteLine("{0} is prime factor of {1}", i, Number);
+    }
+
+}
+Console.ReadLine();
