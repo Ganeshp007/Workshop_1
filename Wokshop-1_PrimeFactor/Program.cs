bc292c6e22c79377a55957a4638acdc218199bcc
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-28 10:26:41 +0530

    Modified: Problem _FInd_PrimeFactor

--- Wokshop-1_PrimeFactor/Program.cs
+++ Wokshop-1_PrimeFactor/Program.cs
@@ -3,12 +3,19 @@ Console.WriteLine("Hello, World!");
 int i;
 Console.WriteLine("Enter the number to find prime factors:-");
 int Number=Convert.ToInt32(Console.ReadLine());
-for(i = 1; i <= Number; i++)
+if(Number<=32)
 {
-    if(Number%i==0)
+    for (i = 1; i <= Number; i++)
     {
-        Console.WriteLine("{0} is prime factor of {1}", i, Number);
-    }
+        if (Number % i == 0)
+        {
+            Console.WriteLine("{0} is prime factor of {1}", i, Number);
+        }
 
+    }
+}
+else
+{
+    Console.WriteLine("The number should be less than 32!!");
 }
 Console.ReadLine();
